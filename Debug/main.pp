b0957908f4f65eb0d0a476c0dbfac610e57cac49
Ursus Schneider <develop@ursus.cc>
2016-01-17 20:06:30 +0100

    Working

---
+++ Debug/main.pp
@@ -0,0 +1,21 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/intrinsics.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/intrinsics_legacy_undefs.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/stdio.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/linkage.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/stdarg.h
+
+../main.c: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/stdio.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.6/include/stdarg.h: 
--- main.c
+++ main.c
@@ -1,10 +1,59 @@
 #include <msp430.h> 
+#include <stdio.h>
 
+// what info am I keeping per thread
+typedef struct _thread {
+	void (*funcPtr)();
+} thread_t;
+
+char output [10];
+unsigned int outputPos;
+
+// one function
+void function1 () {
+	output [outputPos++] = '1';
+}
+// one function
+void function2 () {
+	output [outputPos++] = '2';
+}
 /*
  * main.c
  */
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-	
+
+    // variable you use to test
+    unsigned int i;
+
+    // array to hold the functions
+    thread_t threadList [10];
+    for (i = 0; i < 10; i++)
+    	if ((i % 2) == 0)
+    		threadList [i].funcPtr = &function2;
+    	else
+    		threadList [i].funcPtr = &function1;
+
+    // clear the output array
+    for (i = 0; i < 10; i++)
+    	output [i] = ' ';
+    outputPos = 0;
+
+    // this fills output with 1 / 2
+    for (i = 0; i < 5; i++) {
+    	function1();
+    	function2();
+    }
+
+    // clear the array again
+    for (i = 0; i < 10; i++)
+    	output [i] = ' ';
+    outputPos = 0;
+
+    // now fill the array with pointers
+    for (i = 0; i < 10; i++)
+    	threadList [i].funcPtr();
+
+
 	return 0;
 }
